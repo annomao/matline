bbf8ad48c4366aa53fc4b36d764dc6f7e04c9de6
annomao <omaoannkerubo@gmail.com>
2022-05-24 01:37:55 +0300

    add about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 
 const About = () =>{
   return(
-    <></>
+    <section className="text-center">
+      <h1 className="font-bold text-4xl text-app-maroon/90 poppins mb-3">About</h1>
+      <div className="px-3 md:px-5 text-xs md:text-sm poppins">
+      MatLine is a local public transport company that facilitates the movement of passengers across the country.
+      We operate to and from major cities. Established in the early 2000's, we pride ourselves in being efficient.
+      We value our clients and hence strive to make it convinient for them to use our services.
+      </div>
+    </section>
   )
 }
 export default About
