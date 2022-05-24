3362692315e0f1cf703fa1e0652549162ba85193
annomao <omaoannkerubo@gmail.com>
2022-05-24 03:03:04 +0300

    add testimonial section

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () =>{
   return(
-    <section className="text-center">
-      <h1 className="font-bold text-4xl text-app-maroon/90 poppins mb-3">About</h1>
-      <div className="px-3 md:px-5 text-xs md:text-sm poppins">
+    <section className="text-center my-10">
+      <h1 className="font-bold text-4xl text-app-maroon/90 poppins mb-10">About</h1>
+      <div className="px-3 md:px-5 text-sm md:text-base poppins">
       MatLine is a local public transport company that facilitates the movement of passengers across the country.
       We operate to and from major cities. Established in the early 2000's, we pride ourselves in being efficient.
       We value our clients and hence strive to make it convinient for them to use our services.
---
+++ src/components/TestimonialCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TestimonialCard = ({image,name,text}) => {
+  return(
+    <div className="rounded shadow-2xl w-56">
+    <img className="object-cover h-48 w-56" src={image}/>
+    <span className="py-3 pl-2 font-thin leading-3 poppins">{name}</span>
+    <p className="p-3 text-sm">{text}</p>
+    </div>
+  )
+}
+
+export default TestimonialCard
