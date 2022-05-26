d081291100202d3026b411eddba4af64261355a2
annomao <omaoannkerubo@gmail.com>
2022-05-26 14:11:15 +0300

    add seat selection functionality

--- src/components/dashboard/BookingPage.js
+++ src/components/dashboard/BookingPage.js
@@ -12,11 +12,48 @@ const BookingPage = () => {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
+  const [telephone, setTelephone] = useState(0)
+  const [seats, setSeats] = useState([])
+  const [userError, setUserError] = useState([])
 
+  // Check if user is logged in
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/login");
   }, [user, loading,navigate]);
+
+  // function to get seat number
+  const getSeatNumber = (seat) => {
+    if(seats.includes(seat)){
+      const newArray = seats.filter(s => s !== seat)
+      setSeats(newArray)
+    }else{
+      setSeats([...seats,seat])
+    }
+    
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!(telephone.match('[0-9]{10}'))){
+      setUserError(["Enter Valid Phone Number!"]);
+    }else if(seats.length<=0){
+      setUserError(["Please choose a seat Number!"]);
+    }else{
+      const formData = { 
+        userid:user.uid,
+        phone: telephone, 
+        seats: seats , 
+        bookingDate:startDate.toDateString()
+      };
+      console.log(formData,user)
+      setSeats([])
+      setStartDate(new Date())
+      setTelephone("")
+      setUserError([])
+    }
+  }
+  
 
   return(
     <>
@@ -29,8 +66,9 @@ const BookingPage = () => {
             <label className="block uppercase tracking-wide text-app-maroon text-xs font-bold mb-2">
               Telephone Number
             </label>
-            <input className="appearance-none block w-full bg-gray-200 text-app-maroon border border-app-maroon rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="tel"/>
-            <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+            <input className="appearance-none block w-full bg-gray-200 text-app-maroon border border-app-maroon rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
+            value={telephone} onChange={(e)=> setTelephone(e.target.value)}
+            type="tel"/>
           </div>
           <div className="w-full md:w-1/2 px-3">
             <label className="block uppercase tracking-wide text-app-maroon text-xs font-bold mb-2">
@@ -45,17 +83,25 @@ const BookingPage = () => {
             />
           </div>
         </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button className="shadow bg-app-maroon/90 hover:bg-app-maroon focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded" type="button">
-            Next
+        <SeatSelection addSeat={getSeatNumber}/> 
+        {userError.length > 0
+        ? userError.map((error, index) => (
+          <p key={index} className="tracking-wide text-app-maroon text-xs font-bold mb-2">
+            {error}
+          </p>
+        ))
+      : null}
+        <div className="flex justify-center mt-8">
+          <div>
+            <button className="shadow bg-app-maroon/90 hover:bg-app-maroon focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded" 
+            onClick={handleSubmit}
+            type="button">
+            BOOK
             </button>
           </div>
         </div>
         </form>
       </div> 
-      <SeatSelection/> 
     </section>
     </>
     
--- src/components/dashboard/Seat.js
+++ src/components/dashboard/Seat.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
-const Seat = ({label})=>{
+const Seat = ({label,addSeat})=>{
+  const[disable,setDisable] = useState(false)
+
+  const enabledClassName = "h-24 w-28 bg-app-maroon/90"
+  const disabledClassName = "h-24 w-28 bg-app-maroon/50"
+
+  const handleClick = (e)=> {
+    e.preventDefault()
+    addSeat(e.target.textContent)
+    setDisable((disable)=> !disable)
+  }
   return(
-    <button className="h-24 w-28 bg-app-maroon/90">
+    <button className={disable? disabledClassName: enabledClassName }
+    onClick={handleClick }>
       {label}
     </button>
   )
